46500eea82ec116bb210b7e724915102cba10e13
Ailwei <Ailweimaemu@gmail.com>
2023-10-18 17:40:38 -0700

    5. nop

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop_func - No operation, does nothing.
+ * @stack: Pointer to the stack.
+ * @h: Line number in the Monty file.
+ *
+ * Return: No return value.
+ */
+void nop_func(stack_t **stack, unsigned int h)
+{
+    (void)stack;
+    (void)h;
+}
+
